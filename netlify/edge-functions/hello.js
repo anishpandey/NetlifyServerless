0eb6d1f2d8e196cee7892c5aed54f440263576ad
Anish Kumar <pandey.anish@gmail.com>
2023-09-07 16:57:43 +0530

    updated

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,4 +1,4 @@
-export default async (request) => {
+export default async (request, context) => {
     return new Response("Hello, World!", {
       headers: { "content-type": "text/html" },
     });
